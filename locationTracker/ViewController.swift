e41b7d4a7d687da9f09903d6ecaf558caa036110
rutwik.shinde <rutwik.shinde@tatadigital.com>
2022-03-05 15:14:56 +0530

    Initial Commit

---
+++ locationTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  locationTracker
+//
+//  Created by Rutwik Shinde on 05/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
